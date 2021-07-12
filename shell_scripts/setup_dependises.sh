1e01e3e61f8a74ed9d191cd140676534bebecb61
IvanSnap360 <ivpetr2003@gmail.com>
2021-07-12 10:19:49 +0300

    fix some changes

---
+++ shell_scripts/setup_dependises.sh
@@ -0,0 +1,10 @@
+
+
+
+#rosserial
+sudo apt install ros-$ROS_DISTRO-rosserial -y
+sudo apt install ros-$ROS_DISTRO-rosserial-arduino -y
+
+#kml_parser
+sudo apt-get install libxml2-dev libxslt-dev
+pip install pykml

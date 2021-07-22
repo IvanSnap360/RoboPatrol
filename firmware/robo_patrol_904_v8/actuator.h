2800c686cf627e6335aa02dcf8ae150ef2189ac0
IvanSnap360_Note <ivpetr2003@gmail.com>
2021-07-22 11:15:59 +0300

    sysn commit

--- firmware/robo_patrol_904_v8/actuator.h
+++ firmware/robo_patrol_904_v8/actuator.h
@@ -17,16 +17,36 @@ private:
     int _pwm_cntrl;
 
     int _rev_tiks;
+    long prev_encoder_ticks_;
 
     int _dir;
 
     float _wd;
 
     unsigned long spin_last_time;
+    unsigned long prev_update_time_;
+
+    float _rpm;
 
     int getMotorValue(float value)
     {
         return map(value * 100, -(2 * M_PI * 100), (2 * M_PI * 100), -255, 255);
+    }
+
+    void calcRPM()
+    {
+        unsigned long current_time = millis();
+        unsigned long dt = current_time - prev_update_time_;
+
+        //convert the time from milliseconds to minutes
+        double dtm = (double)dt / 60000;
+        double delta_ticks = rel_tiks2 - prev_encoder_ticks_;
+
+        //calculate wheel's speed (RPM)
+        _rpm = (delta_ticks / _rev_tiks) / dtm;
+
+        prev_update_time_ = current_time;
+        prev_encoder_ticks_ = rel_tiks2;
     }
 
 public:
@@ -34,6 +54,7 @@ public:
     void setPins(int m_pin1, int m_pin2, int enc_pin);
     void setTiksPerRevolution(unsigned long tiks);
     void setWheelDeametr(float d);
+    float getRPM();
     void writeAngularVelocity(float vel);
     void setMinPWMvalue(int min_pwm_signal);
     void setPWMRanges(int min_pwm, int max_pwm);
@@ -42,6 +63,7 @@ public:
 
     int _enc_pin;
     long rel_tiks;
+    long rel_tiks2;
     unsigned long abs_tiks;
 
     float impulse2meters(float x)
--- firmware/robo_patrol_904_v8/robo_patrol_904_v8.ino
+++ firmware/robo_patrol_904_v8/robo_patrol_904_v8.ino
@@ -13,10 +13,10 @@ ros::NodeHandle_<ROS_HARDWARE, ROS_MAX_SUBSCRIBERS, ROS_MAX_PUBLISHERS, ROS_MAX_
 
 void joint_control_cb_f(const sensor_msgs::JointState &joint_data)
 {
-    M1.writeAngularVelocity(joint_data.velocity[0]);
-    M2.writeAngularVelocity(joint_data.velocity[1]);
-    M3.writeAngularVelocity(joint_data.velocity[2]);
-    M4.writeAngularVelocity(joint_data.velocity[3]);
+  M1.writeAngularVelocity(joint_data.velocity[0]);
+  M2.writeAngularVelocity(joint_data.velocity[1]);
+  M3.writeAngularVelocity(joint_data.velocity[2]);
+  M4.writeAngularVelocity(joint_data.velocity[3]);
 }
 
 ros::Subscriber<sensor_msgs::JointState> joint_control_sub("/joint_contol", &joint_control_cb_f);
@@ -40,150 +40,178 @@ unsigned long jont_state_last_publish_time = 0;
 void setup()
 {
 
-    M1.setPins(M1_MOTOR_PIN_1, M1_MOTOR_PIN_2, M1_ENCODER_PIN);
-    M1.setWheelDeametr(ROBOT_WHEEL_DEAMETR);
-    M1.setTiksPerRevolution(MOTOR_TIKS_PER_REVOLUTION);
-    M1.setMinPWMvalue(MIN_PWM_SIGNAL);
-    M1.setPWMRanges(MIN_PWM, MAX_PWM);
-    attachInterrupt(M1._enc_pin, M1_enc_f, MOTOR_ENCODER_READ_MODE);
+  M1.setPins(M1_MOTOR_PIN_1, M1_MOTOR_PIN_2, M1_ENCODER_PIN);
+  M1.setWheelDeametr(ROBOT_WHEEL_DEAMETR);
+  M1.setTiksPerRevolution(MOTOR_TIKS_PER_REVOLUTION);
+  M1.setMinPWMvalue(MIN_PWM_SIGNAL);
+  M1.setPWMRanges(MIN_PWM, MAX_PWM);
+  attachInterrupt(M1._enc_pin, M1_enc_f, MOTOR_ENCODER_READ_MODE);
 
-    M2.setPins(M2_MOTOR_PIN_1, M2_MOTOR_PIN_2, M2_ENCODER_PIN);
-    M2.setWheelDeametr(ROBOT_WHEEL_DEAMETR);
-    M2.setTiksPerRevolution(MOTOR_TIKS_PER_REVOLUTION);
-    M2.setMinPWMvalue(MIN_PWM_SIGNAL);
-    M2.setPWMRanges(MIN_PWM, MAX_PWM);
-    attachInterrupt(M2._enc_pin, M2_enc_f, MOTOR_ENCODER_READ_MODE);
+  M2.setPins(M2_MOTOR_PIN_1, M2_MOTOR_PIN_2, M2_ENCODER_PIN);
+  M2.setWheelDeametr(ROBOT_WHEEL_DEAMETR);
+  M2.setTiksPerRevolution(MOTOR_TIKS_PER_REVOLUTION);
+  M2.setMinPWMvalue(MIN_PWM_SIGNAL);
+  M2.setPWMRanges(MIN_PWM, MAX_PWM);
+  attachInterrupt(M2._enc_pin, M2_enc_f, MOTOR_ENCODER_READ_MODE);
 
-    M3.setPins(M3_MOTOR_PIN_1, M3_MOTOR_PIN_2, M3_ENCODER_PIN);
-    M3.setWheelDeametr(ROBOT_WHEEL_DEAMETR);
-    M3.setTiksPerRevolution(MOTOR_TIKS_PER_REVOLUTION);
-    M3.setMinPWMvalue(MIN_PWM_SIGNAL);
-    M3.setPWMRanges(MIN_PWM, MAX_PWM);
-    attachInterrupt(M3._enc_pin, M3_enc_f, MOTOR_ENCODER_READ_MODE);
+  M3.setPins(M3_MOTOR_PIN_1, M3_MOTOR_PIN_2, M3_ENCODER_PIN);
+  M3.setWheelDeametr(ROBOT_WHEEL_DEAMETR);
+  M3.setTiksPerRevolution(MOTOR_TIKS_PER_REVOLUTION);
+  M3.setMinPWMvalue(MIN_PWM_SIGNAL);
+  M3.setPWMRanges(MIN_PWM, MAX_PWM);
+  attachInterrupt(M3._enc_pin, M3_enc_f, MOTOR_ENCODER_READ_MODE);
 
-    M4.setPins(M4_MOTOR_PIN_1, M4_MOTOR_PIN_2, M4_ENCODER_PIN);
-    M4.setWheelDeametr(ROBOT_WHEEL_DEAMETR);
-    M4.setTiksPerRevolution(MOTOR_TIKS_PER_REVOLUTION);
-    M4.setMinPWMvalue(MIN_PWM_SIGNAL);
-    M4.setPWMRanges(MIN_PWM, MAX_PWM);
-    attachInterrupt(M4._enc_pin, M4_enc_f, MOTOR_ENCODER_READ_MODE);
+  M4.setPins(M4_MOTOR_PIN_1, M4_MOTOR_PIN_2, M4_ENCODER_PIN);
+  M4.setWheelDeametr(ROBOT_WHEEL_DEAMETR);
+  M4.setTiksPerRevolution(MOTOR_TIKS_PER_REVOLUTION);
+  M4.setMinPWMvalue(MIN_PWM_SIGNAL);
+  M4.setPWMRanges(MIN_PWM, MAX_PWM);
+  attachInterrupt(M4._enc_pin, M4_enc_f, MOTOR_ENCODER_READ_MODE);
 
-    state_msg.header.frame_id = "/driver_states";
-    state_msg.name_length = MOTOR_COUNT;
-    state_msg.velocity_length = MOTOR_COUNT;
-    state_msg.position_length = MOTOR_COUNT;
-    state_msg.effort_length = MOTOR_COUNT;
-    state_msg.name = state_names;
-    state_msg.position = state_pos;
-    state_msg.velocity = state_vel;
-    state_msg.effort = state_eff;
+  state_msg.header.frame_id = "/driver_states";
+  state_msg.name_length = MOTOR_COUNT;
+  state_msg.velocity_length = MOTOR_COUNT;
+  state_msg.position_length = MOTOR_COUNT;
+  state_msg.effort_length = MOTOR_COUNT;
+  state_msg.name = state_names;
+  state_msg.position = state_pos;
+  state_msg.velocity = state_vel;
+  state_msg.effort = state_eff;
 
-    nh.initNode();
-    nh.advertise(joint_state_pub);
-    nh.subscribe(joint_control_sub);
+  nh.initNode();
+  nh.advertise(joint_state_pub);
+  nh.subscribe(joint_control_sub);
 }
 
 unsigned long last_blink_time;
 
 void loop()
 {
-    if (nh.connected())
+  if (nh.connected())
+  {
+    if (millis() - last_blink_time > 1000)
     {
-        if (millis() - last_blink_time > 1000)
-        {
-            toggleLED();
-            last_blink_time = millis();
-        }
-        unsigned long t = millis() - jont_state_last_publish_time;
-        if (t > (1000.0 / JOINTS_STATE_PUBLISH_RATE_HZ))
-        {
-            state_pos[0] = M1.impulse2meters(M1.abs_tiks);
-            state_pos[1] = M2.impulse2meters(M2.abs_tiks);
-            state_pos[2] = M3.impulse2meters(M3.abs_tiks);
-            state_pos[3] = M4.impulse2meters(M4.abs_tiks);
-
-            state_vel[0] = M1.impulse2rad(M1.rel_tiks / ((float)t / 1000.0));
-            state_vel[1] = M2.impulse2rad(M2.rel_tiks / ((float)t / 1000.0));
-            state_vel[2] = M3.impulse2rad(M3.rel_tiks / ((float)t / 1000.0));
-            state_vel[3] = M4.impulse2rad(M4.rel_tiks / ((float)t / 1000.0));
-
-            state_msg.header.stamp = nh.now();
-
-            joint_state_pub.publish(&state_msg);
-
-            jont_state_last_publish_time = millis();
-
-            M1.rel_tiks = 0;
-            M2.rel_tiks = 0;
-            M3.rel_tiks = 0;
-            M4.rel_tiks = 0;
-        }
-        // my_main();
+      toggleLED();
+      last_blink_time = millis();
     }
-    else
+    unsigned long t = millis() - jont_state_last_publish_time;
+    if (t > (1000.0 / JOINTS_STATE_PUBLISH_RATE_HZ))
     {
-        if (millis() - last_blink_time > 100)
-        {
-            toggleLED();
-            last_blink_time = millis();
-        }
-        M1.writeAngularVelocity(0.0);
-        M2.writeAngularVelocity(0.0);
-        M3.writeAngularVelocity(0.0);
-        M4.writeAngularVelocity(0.0);
+      state_pos[0] = M1.impulse2meters(M1.abs_tiks);
+      state_pos[1] = M2.impulse2meters(M2.abs_tiks);
+      state_pos[2] = M3.impulse2meters(M3.abs_tiks);
+      state_pos[3] = M4.impulse2meters(M4.abs_tiks);
+
+      state_vel[0] = M1.impulse2rad(M1.rel_tiks * ((float)1000.0 / t));
+      state_vel[1] = M2.impulse2rad(M2.rel_tiks * ((float)1000.0 / t));
+      state_vel[2] = M3.impulse2rad(M3.rel_tiks * ((float)1000.0 / t));
+      state_vel[3] = M4.impulse2rad(M4.rel_tiks * ((float)1000.0 / t));
+
+      state_eff[0] = M1.getRPM();
+      state_eff[1] = M2.getRPM();
+      state_eff[2] = M3.getRPM();
+      state_eff[3] = M4.getRPM();
+
+      state_msg.header.stamp = nh.now();
+
+      joint_state_pub.publish(&state_msg);
+
+      jont_state_last_publish_time = millis();
+
+      M1.rel_tiks = 0;
+      M2.rel_tiks = 0;
+      M3.rel_tiks = 0;
+      M4.rel_tiks = 0;
     }
-    M1.spin(10);
-    M2.spin(10);
-    M3.spin(10);
-    M4.spin(10);
-    nh.spinOnce();
+    // my_main();
+  }
+  else
+  {
+    if (millis() - last_blink_time > 100)
+    {
+      toggleLED();
+      last_blink_time = millis();
+    }
+    M1.writeAngularVelocity(0.0);
+    M2.writeAngularVelocity(0.0);
+    M3.writeAngularVelocity(0.0);
+    M4.writeAngularVelocity(0.0);
+
+    M1.abs_tiks = 0;
+    M2.abs_tiks = 0;
+    M3.abs_tiks = 0;
+    M4.abs_tiks = 0;
+
+    M1.rel_tiks = 0;
+    M2.rel_tiks = 0;
+    M3.rel_tiks = 0;
+    M4.rel_tiks = 0;
+
+    M1.rel_tiks2 = 0;
+    M2.rel_tiks2 = 0;
+    M3.rel_tiks2 = 0;
+    M4.rel_tiks2 = 0;
+  }
+  M1.spin(50);
+  M2.spin(50);
+  M3.spin(50);
+  M4.spin(50);
+  nh.spinOnce();
 }
 
 void M1_enc_f()
 {
-    if (M1.getDirection() > 0)
-    {
-        M1.rel_tiks++;
-    }
-    else if (M1.getDirection() < 0)
-    {
-        M1.rel_tiks--;
-    }
-    M1.abs_tiks++;
+  if (M1.getDirection() > 0)
+  {
+    M1.rel_tiks++;
+    M1.rel_tiks2++;
+  }
+  else if (M1.getDirection() < 0)
+  {
+    M1.rel_tiks--;
+    M1.rel_tiks2--;
+  }
+  M1.abs_tiks++;
 }
 void M2_enc_f()
 {
-    if (M2.getDirection() > 0)
-    {
-        M2.rel_tiks++;
-    }
-    else if (M2.getDirection() < 0)
-    {
-        M2.rel_tiks--;
-    }
-    M2.abs_tiks++;
+  if (M2.getDirection() > 0)
+  {
+    M2.rel_tiks++;
+    M2.rel_tiks2++;
+  }
+  else if (M2.getDirection() < 0)
+  {
+    M2.rel_tiks--;
+    M2.rel_tiks2--;
+  }
+  M2.abs_tiks++;
 }
 void M3_enc_f()
 {
-    if (M3.getDirection() > 0)
-    {
-        M3.rel_tiks++;
-    }
-    else if (M3.getDirection() < 0)
-    {
-        M3.rel_tiks--;
-    }
-    M3.abs_tiks++;
+  if (M3.getDirection() > 0)
+  {
+    M3.rel_tiks++;
+    M3.rel_tiks2++;
+  }
+  else if (M3.getDirection() < 0)
+  {
+    M3.rel_tiks--;
+    M3.rel_tiks2--;
+  }
+  M3.abs_tiks++;
 }
 void M4_enc_f()
 {
-    if (M4.getDirection() > 0)
-    {
-        M4.rel_tiks++;
-    }
-    else if (M4.getDirection() < 0)
-    {
-        M4.rel_tiks--;
-    }
-    M4.abs_tiks++;
+  if (M4.getDirection() > 0)
+  {
+    M4.rel_tiks++;
+    M4.rel_tiks2++;
+  }
+  else if (M4.getDirection() < 0)
+  {
+    M4.rel_tiks--;
+    M4.rel_tiks2--;
+  }
+  M4.abs_tiks++;
 }
